c151d2ca1361d06913feb311ee4dd655833bff2f
MZMcBride <public@mzmcbride.com>
2009-04-05 04:29:00 -0400

    added longstubs.py

---
+++ longstubs.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2008 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = 'Wikipedia:Database reports/Long stubs'
+
+report_template = u'''
+Pages in categories that end in "stubs" and whose page length is greater than \
+20,000 bytes; data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! Page
+! Length
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki()
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host='sql-s1', db='enwiki_p', read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* longstubs.py SLOW_OK */
+SELECT DISTINCT
+  page_title,
+  page_len
+FROM page
+JOIN categorylinks
+ON cl_from = page_id
+WHERE cl_to LIKE "%stubs"
+AND page_len > 20000
+AND page_namespace = 0;
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    page_title = u'[[%s]]' % unicode(row[0], 'utf-8')
+    page_len = row[1]
+    table_row = u'''| %d
+| %s
+| %s
+|-''' % (i, page_title, page_len)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary='updated page')
+
+cursor.close()
+conn.close()
