b33f240989fc13b6c2bb8c4ba2dfdab24a097c30
MZMcBride <public@mzmcbride.com>
2009-04-15 02:09:07 -0400

    added mosttransclusions.py

---
+++ mosttransclusions.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2008 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = 'Wikipedia:Database reports/Templates with the most transclusions'
+
+report_template = u'''
+Templates with the most transclusions (limited to the first 1000 entries); data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! Template
+! Uses
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki()
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host='sql-s1', db='enwiki_p', read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* mosttransclusions.py SLOW_OK */
+SELECT
+  tl_title,
+  COUNT(*)
+FROM templatelinks
+WHERE tl_namespace = 10
+GROUP BY tl_title
+ORDER BY COUNT(*) DESC
+LIMIT 1000;
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    tl_title = u'[[Template:%s]]' % unicode(row[0], 'utf-8')
+    uses = row[1]
+    table_row = u'''| %d
+| %s
+| %s
+|-''' % (i, tl_title, uses)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary='updated page')
+
+cursor.close()
+conn.close()
