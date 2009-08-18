e9495008f289135b04a307954762c9240463f58c
MZMcBride <public@mzmcbride.com>
2009-08-18 18:00:52 -0400

    Added overusednonfree.py

---
+++ overusednonfree.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2008 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'Overused non-free files'
+
+report_template = u'''
+Non-free files used on more than 8 pages; data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! File
+! Uses
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* overusednonfree.py SLOW_OK */
+SELECT
+  ns_name,
+  page_title,
+  COUNT(*)
+FROM imagelinks
+JOIN (SELECT
+        page_id,
+        ns_name,
+        page_title
+      FROM page
+      JOIN toolserver.namespace
+      ON dbname = 'enwiki_p'
+      AND page_namespace = ns_id
+      JOIN categorylinks
+      ON cl_from = page_id
+      WHERE cl_to = 'All_non-free_media'
+      AND page_namespace = 6) AS pgtmp
+ON pgtmp.page_title = il_to
+GROUP BY il_to
+HAVING COUNT(*) > 8
+ORDER BY COUNT(*) ASC;
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    ns_name = row[0]
+    page_title = '[[:%s:%s|%s]]' % (ns_name, unicode(row[1], 'utf-8'), unicode(row[1], 'utf-8'))
+    count = row[2]
+    table_row = u'''| %d
+| %s
+| %s
+|-''' % (i, page_title, count)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
