a0f18847b52f512b5672af984ae87fd159f85dec
MZMcBride <public@mzmcbride.com>
2009-11-09 19:16:12 -0500

    Updated brokenredirects.py to use a standard output (without judgment of the entries) and switched the report to use the standard bot.

--- brokenredirects.py
+++ brokenredirects.py
@@ -19,15 +19,13 @@ import datetime
 import re
 import time
 import MySQLdb
-import wikipedia
+import wikitools
 import settings
 
 report_title = settings.rootpage + 'Broken redirects'
 
 report_template = u'''
 Broken redirects; data as of <onlyinclude>%s</onlyinclude>.
-
-Colored rows have been reviewed.
 
 {| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
@@ -37,10 +35,8 @@ Colored rows have been reviewed.
 |}
 '''
 
-delete = False
-sleep_time = 0
-
-site = wikipedia.getSite()
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
 
 conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
 cursor = conn.cursor()
@@ -67,48 +63,15 @@ ORDER BY p1.page_namespace ASC;
 i = 1
 output = []
 for row in cursor.fetchall():
-    redirect = wikipedia.Page(site, '%s:%s' % (row[1], unicode(row[2], 'utf-8')))
-    page_namespace = row[0]
     ns_name = u'%s' % unicode(row[1], 'utf-8')
     page_title = u'%s' % unicode(row[2], 'utf-8')
+    page_namespace = row[0]
     if page_namespace == 6 or page_namespace == 14:
         page_title = ':%s:%s' % (ns_name, page_title)
     elif ns_name:
         page_title = '%s:%s' % (ns_name, page_title)
     else:
         page_title = '%s' % (page_title)
-    
-    if page_namespace == 6:
-        pass
-
-    elif page_namespace in (2,3) and not re.search(r'/', row[2], re.I|re.U):
-        pass
-
-    elif redirect.isRedirectPage():
-        try:
-            target = redirect.getRedirectTarget()
-            lastedit = datetime.datetime.strptime(redirect.editTime(), '%Y%m%d%H%M%S')
-            if not target.exists() and datetime.datetime.utcnow() - lastedit > datetime.timedelta(days=4) \
-              and not re.search(r'(CAT:)', row[2], re.U):
-                try:
-                    if delete:
-                        redirect.delete('[[WP:CSD#G8|CSD G8]]: [[%s]]' % target.title(), False, False)
-                        time.sleep(sleep_time)
-                        continue
-                    else:
-                        target = target.title()
-                        table_row = u'''|- style="background:#EBE3F4;"
-| %d
-| [[%s]] {{ddbr|1=%s|2=%s}}''' % (i, page_title, page_title, target)
-                        output.append(table_row)
-                        i += 1
-                        continue
-                except wikipedia.BadTitle:
-                    print 'Skipped [[en:%s]]: malformed redirect' % redirect.title()
-                    continue
-        except:
-            print 'Skipped [[en:%s]]: malformed redirect' % redirect.title()
-            continue
     table_row = u'''|-
 | %d
 | [[%s]]''' % (i, page_title)
@@ -119,9 +82,10 @@ cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM rece
 rep_lag = cursor.fetchone()[0]
 current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
 
-report = wikipedia.Page(site, report_title)
-report.put(report_template % (current_of, '\n'.join(output)), settings.editwhip, True, False)
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
 cursor.close()
 conn.close()
-
-wikipedia.stopme()
