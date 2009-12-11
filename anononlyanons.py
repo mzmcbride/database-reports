3f87e4cc750df4e81b58b74dfdb6bb2e77cb0da6
MZMcBride <public@mzmcbride.com>
2009-12-11 09:58:58 -0500

    Added anononlyanons.py

---
+++ anononlyanons.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2009 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'Short user talk pages for IPs'
+
+report_template = u'''
+Users talk pages of anonymous users where the only contributors to the page \
+are anonymous, the page is less than 50 bytes in length, and it contains no \
+templates (limited to the first 1000 entries); data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! Page
+! Length
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* anononlyanons.py SLOW_OK */
+SELECT DISTINCT
+  ns_name,
+  page_title,
+  page_len
+FROM page
+JOIN toolserver.namespace
+ON dbname = 'enwiki_p'
+AND page_namespace = ns_id
+LEFT JOIN templatelinks
+ON tl_from = page_id
+JOIN revision
+ON rev_page = page_id
+WHERE page_namespace = 3
+AND page_title RLIKE %s
+AND NOT EXISTS (SELECT
+                  1
+                FROM revision
+                WHERE rev_page = page_id
+                AND rev_user != 0)
+AND page_len < 50
+AND ISNULL(tl_from)
+LIMIT 1000;
+''' , r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    ns_name = u'%s' % unicode(row[0], 'utf-8')
+    page_title = u'%s' % unicode(row[1], 'utf-8')
+    full_page_title = u'[[%s:%s|%s]]' % (ns_name, page_title, page_title)
+    page_len = row[2]
+    table_row = u'''| %d
+| %s
+| %s
+|-''' % (i, full_page_title, page_len)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
