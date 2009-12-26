604a0bb1b05bff0c223874d36bcf564294c42efc
MZMcBride <public@mzmcbride.com>
2009-12-25 20:46:05 -0500

    Added potenshbdps3.py

---
+++ potenshbdps3.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2009 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'Potential biographies of dead people (3)'
+
+report_template = u'''
+Articles in a "XXXX deaths" category whose talk pages transclude {{tl|BLP}}; \
+data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! Biography
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* potenshbdps3.py SLOW_OK */
+SELECT
+  pg1.page_title
+FROM page AS pg1
+JOIN templatelinks
+ON pg1.page_id = tl_from
+WHERE tl_namespace = 10
+AND tl_title = 'BLP'
+AND pg1.page_namespace = 1
+AND EXISTS (SELECT
+              1
+            FROM page AS pg2
+            JOIN categorylinks
+            ON pg2.page_id = cl_from
+            WHERE pg1.page_title = pg2.page_title
+            AND pg2.page_namespace = 0
+            AND cl_to RLIKE '^[0-9]{1,4}_deaths$');
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    page_title = u'[[%s]]' % unicode(row[0], 'utf-8')
+    table_row = u'''| %d
+| %s
+|-''' % (i, page_title)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
