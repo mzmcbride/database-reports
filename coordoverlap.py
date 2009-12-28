ed6901028c117f40337847c38e58f7d461102f38
MZMcBride <public@mzmcbride.com>
2009-12-28 02:07:04 -0500

    Added coordoverlap.py

---
+++ coordoverlap.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2009 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'Articles containing overlapping coordinates'
+
+report_template = u'''
+Articles that contain {{tl|Coord/display/inline,title}} and {{tl|Coord/display/title}}; \
+data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! Article
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* coordoverlap.py SLOW_OK */
+SELECT
+  page_title
+FROM page
+JOIN templatelinks AS tmp1
+ON tmp1.tl_from = page_id
+JOIN templatelinks AS tmp2
+ON tmp2.tl_from = page_id
+WHERE page_namespace = 0
+AND tmp1.tl_namespace = 10
+AND tmp1.tl_title = 'Coord/display/inline,title'
+AND tmp2.tl_namespace = 10
+AND tmp2.tl_title = 'Coord/display/title';
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    page_title = '[[%s]]' % unicode(row[0], 'utf-8')
+    table_row = u'''| %d
+| %s
+|-''' % (i, page_title)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
