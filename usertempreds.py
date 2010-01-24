55c912dff546efaecc91e2454394afaab1368af0
MZMcBride <public@mzmcbride.com>
2010-01-23 23:48:29 -0500

    Added usertempreds.py

---
+++ usertempreds.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2009 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'User template redirects'
+
+report_template = u'''
+Pages in the Template namespace that redirect to the User namespace; \
+data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! Template
+! Target
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* usertempreds.py SLOW_OK */
+SELECT
+  ns_name,
+  page_title,
+  rd_title
+FROM page
+JOIN toolserver.namespace
+ON dbname = 'enwiki_p'
+AND ns_id = page_namespace
+JOIN redirect
+ON rd_from = page_id
+WHERE rd_namespace = 2
+AND page_namespace = 10;
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    ns_name = u'%s' % unicode(row[0], 'utf-8')
+    page_title = u'%s' % unicode(row[1], 'utf-8')
+    rd_title = u'%s' % unicode(row[2], 'utf-8')
+    full_page_title = u'[[%s:%s|%s]]' % (ns_name, page_title, page_title)
+    full_rd_title = u'[[User:%s]]' % (rd_title)
+    table_row = u'''| %d
+| %s
+| %s
+|-''' % (i, full_page_title, full_rd_title)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
