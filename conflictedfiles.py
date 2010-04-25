d7b5239671b04c4ba31be862cad2857bf8bc3a1e
MZMcBride <g@mzmcbride.com>
2010-04-25 01:08:30 -0400

    Added conflictedfiles.py

---
+++ conflictedfiles.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2010 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'Files with conflicting categorization'
+
+report_template = u'''
+Files that are categorized in [[:Category:All non-free media]] and \
+[[:Category:All free media]]; data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! File
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* conflictedfiles.py SLOW_OK */
+SELECT
+  ns_name,
+  page_title
+FROM page
+JOIN toolserver.namespace
+ON dbname = %s
+AND page_namespace = ns_id
+JOIN categorylinks AS c1
+ON c1.cl_from = page_id
+JOIN categorylinks AS c2
+ON c2.cl_from = page_id
+WHERE page_namespace = 6
+AND c1.cl_to = 'All_free_media'
+AND c2.cl_to = 'All_non-free_media';
+''' , settings.dbname)
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    ns_name = u'%s' % unicode(row[0], 'utf-8')
+    page_title = u'%s' % unicode(row[1], 'utf-8')
+    full_page_title = u'[[:%s:%s|%s]]' % (ns_name, page_title, page_title)
+    table_row = u'''| %d
+| %s
+|-''' % (i, page_title)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
