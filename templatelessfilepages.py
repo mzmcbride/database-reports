8b9efb14883efb5c54ddfc8ebfbea7ff54d31137
MZMcBride <g@mzmcbride.com>
2010-05-09 00:27:27 -0400

    Accidentally committed with a very poor commit message. Not sure how that happened....
    Added a CAST() function to a few column comparisons to account for a bug in which some databases are using varchar and some databases are using varbinary. This is documented at https://jira.toolserver.org/browse/TS-549 and https://bugzilla.wikimedia.org/show_bug.cgi?id=22883

--- templatelessfilepages.py
+++ templatelessfilepages.py
@@ -56,12 +56,12 @@ ON tl_from = pg1.page_id
 WHERE NOT EXISTS (SELECT
                     img_name
                   FROM commonswiki_p.image
-                  WHERE img_name = pg1.page_title)
+                  WHERE img_name = CAST(pg1.page_title AS CHAR))
 AND NOT EXISTS (SELECT
                   1
                 FROM commonswiki_p.page AS pg2
                 WHERE pg2.page_namespace = 6
-                AND pg2.page_title = pg1.page_title
+                AND pg2.page_title = CAST(pg1.page_title AS CHAR)
                 AND pg2.page_is_redirect = 1)
 AND pg1.page_namespace = 6
 AND pg1.page_is_redirect = 0
