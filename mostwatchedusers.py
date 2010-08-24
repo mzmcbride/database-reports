527e87fdb60aa5c62bab6e690d35cd8ea183c697
MZMcBride <g@mzmcbride.com>
2010-08-23 23:45:31 -0400

    Added mostwatchedusers.py

---
+++ mostwatchedusers.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2.5
+
+# Copyright 2010 bjweeks, MZMcBride
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import MySQLdb
+import wikitools
+import settings
+
+report_title = settings.rootpage + 'Most-watched users'
+
+report_template = u'''
+Most-watched users; data as of <onlyinclude>%s</onlyinclude>.
+
+{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+|- style="white-space:nowrap;"
+! No.
+! User
+! Watchers
+|-
+%s
+|}
+'''
+
+wiki = wikitools.Wiki(settings.apiurl)
+wiki.login(settings.username, settings.password)
+
+conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+cursor = conn.cursor()
+cursor.execute('''
+/* mostwatchedusers.py SLOW_OK */
+SELECT
+  wl_title,
+  COUNT(*)
+FROM watchlist
+JOIN page
+ON page_title = wl_title
+AND page_namespace = wl_namespace
+WHERE wl_namespace = 3
+AND wl_title NOT LIKE '%/%'
+GROUP BY wl_title
+ORDER BY COUNT(*) DESC
+LIMIT 1000;
+''')
+
+i = 1
+output = []
+for row in cursor.fetchall():
+    wl_title = unicode(row[0], 'utf-8')
+    watchers = row[1]
+    table_row = u'''| %d
+| %s
+| %s
+|-''' % (i, wl_title, watchers)
+    output.append(table_row)
+    i += 1
+
+cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+rep_lag = cursor.fetchone()[0]
+current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
+
+cursor.close()
+conn.close()
