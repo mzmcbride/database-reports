4ebef637ceecb357646667a767ef628395808157
MZMcBride <g@mzmcbride.com>
2011-05-31 21:02:40 -0400

    * Code cleanup and modernization. * Added function to check if a global account exists. If a global account exists, don't consider the pages ownerless and subsequently do not list them in the report.

--- ownerlessuserpages.py
+++ ownerlessuserpages.py
@@ -1,19 +1,5 @@
-#!/usr/bin/env python2.5
-
-# Copyright 2008 bjweeks, MZMcBride
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#! /usr/bin/env python
+# Public domain; bjweeks, MZMcBride; 2008
 
 import datetime
 import MySQLdb
@@ -22,8 +8,9 @@ import settings
 
 report_title = settings.rootpage + 'Ownerless pages in the user space'
 
-report_template = u'''
-Pages in the user space that do not belong to a [[Special:ListUsers|registered user]]; data as of <onlyinclude>%s</onlyinclude>.
+report_template = u'''\
+Pages in the user space that do not belong to a [[Special:ListUsers|registered user]]; \
+data as of <onlyinclude>%s</onlyinclude>.
 
 {| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
@@ -40,7 +27,24 @@ Pages in the user space that do not belong to a [[Special:ListUsers|registered u
 wiki = wikitools.Wiki(settings.apiurl)
 wiki.login(settings.username, settings.password)
 
-conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
+def has_global_account(cursor2, user):
+    cursor2.execute('''
+    /* ownerlessuserpages.py SLOW_OK */
+    SELECT
+      1
+    FROM globaluser
+    WHERE gu_name = %s;
+    ''' , user)
+    try:
+        if cursor2.fetchone()[0] == 1:
+            return True
+    except TypeError:
+        return False
+    return False
+
+conn = MySQLdb.connect(host=settings.host,
+                       db=settings.dbname,
+                       read_default_file='~/.my.cnf')
 cursor = conn.cursor()
 cursor.execute('''
 /* ownerlessuserpages.py SLOW_OK */
@@ -76,6 +80,10 @@ AND rev_timestamp = (SELECT
 
 i = 1
 output = []
+conn2 = MySQLdb.connect(host='sql-s3',
+                        db='centralauth_p',
+                        read_default_file='~/.my.cnf')
+cursor2 = conn2.cursor()
 for row in cursor.fetchall():
     page_namespace = row[0]
     ns_name = u'%s' % unicode(row[1], 'utf-8')
@@ -89,12 +97,22 @@ for row in cursor.fetchall():
 | %s
 | %s
 |-''' % (i, page_title, page_len, rev_user_text, rev_timestamp)
-    output.append(table_row)
+    if not has_global_account(cursor2, row[2].replace('_', ' ')):
+        output.append(table_row)
     i += 1
+cursor2.close()
+conn2.close()
 
-cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+cursor.execute('''
+               SELECT
+                 UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp)
+               FROM recentchanges
+               ORDER BY rc_timestamp DESC
+               LIMIT 1;
+               ''')
 rep_lag = cursor.fetchone()[0]
-current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+time_diff = datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)
+current_of = time_diff.strftime('%H:%M, %d %B %Y (UTC)')
 
 report = wikitools.Page(wiki, report_title)
 report_text = report_template % (current_of, '\n'.join(output))
