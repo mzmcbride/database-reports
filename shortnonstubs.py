f66181924c4663b4cbb034975342ec84f0238d25
Kunal Mehta <legoktm@gmail.com>
2012-10-06 20:27:57 -0500

    Add No. column, raise limit to 1000 articles, setup for multi-pages (4 total)

--- shortnonstubs.py
+++ shortnonstubs.py
@@ -5,18 +5,20 @@
 
 
 import os
+import math
 import datetime
 import oursql
 import wikitools
 import settings
 
-report_title = settings.rootpage + 'Short non-stubs'
-
+report_title = settings.rootpage + 'Short non-stubs/%i'
+rows_per_page = 250
 report_template = u'''
 Short non-stubs; data as of <onlyinclude>%s</onlyinclude>.
 
 {| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
+! No.
 ! Title
 ! Length
 %s
@@ -42,25 +44,43 @@ AND page_is_redirect = 0
 AND cl_from IS NULL
 AND  page_len < 1500
 ORDER BY page_len ASC
-LIMIT 100;''' , (settings.dbname,))
-
+LIMIT 1000;''' , (settings.dbname,))
+i = 1
 output = []
 for row in cursor.fetchall():
     page_title = u'%s' % unicode(row[0], 'utf-8')
     page_len = u'%s' % unicode(row[1], 'utf-8')
     table_row = u"""|-
+| %d
 | [[%s]]
-| %d""" % (page_title, page_len)
+| %d""" % (i, page_title, page_len)
     output.append(table_row)
+    i+=1
 
 cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
 rep_lag = cursor.fetchone()[0]
 current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
 
-report = wikitools.Page(wiki, report_title)
-report_text = report_template % (current_of, '\n'.join(output))
-report_text = report_text.encode('utf-8')
-report.edit(report_text, summary=settings.editsumm, bot=1)
+end = rows_per_page
+page = 1
+for start in range(0, len(output), rows_per_page):
+    report = wikitools.Page(wiki, report_title % page)
+    report_text = report_template % (current_of, '\n'.join(output[start:end]))
+    report_text = report_text.encode('utf-8')
+    report.edit(report_text, summary=settings.editsumm, bot=1)
+    page += 1
+    end += rows_per_page
+
+page = math.ceil(len(output) / float(rows_per_page)) + 1
+while 1:
+    report = wikitools.Page(wiki, report_title % page)
+    report_text = settings.blankcontent
+    report_text = report_text.encode('utf-8')
+    if not report.exists:
+        break
+    report.edit(report_text, summary=settings.blanksumm, bot=1)
+    page += 1
+
 
 cursor.close()
 conn.close()
