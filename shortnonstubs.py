49295abb036282e2672257bc38c218f9d09b69ee
Kunal Mehta <legoktm@gmail.com>
2012-10-17 00:23:57 -0500

    Convert from paginated report to a single page, limit reduced to 800 results.

--- shortnonstubs.py
+++ shortnonstubs.py
@@ -11,10 +11,9 @@ import oursql
 import wikitools
 import settings
 
-report_title = settings.rootpage + 'Short non-stubs/%i'
-rows_per_page = 250
+report_title = settings.rootpage + 'Short non-stubs'
 report_template = u'''
-Short non-stubs; data as of <onlyinclude>%s</onlyinclude>.
+Short non-stubs (limited to the first 800 results); data as of <onlyinclude>%s</onlyinclude>.
 
 {| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
@@ -42,7 +41,7 @@ AND page_is_redirect = 0
 AND cl_from IS NULL
 AND  page_len < 1500
 ORDER BY page_len ASC
-LIMIT 1000;''' , (settings.dbname,))
+LIMIT 800;''' , (settings.dbname,))
 i = 1
 output = []
 for row in cursor.fetchall():
@@ -51,7 +50,7 @@ for row in cursor.fetchall():
     table_row = u"""|-
 | %d
 | [[%s]]
-| %d""" % (i, page_title, page_len)
+| %s""" % (i, page_title, page_len)
     output.append(table_row)
     i+=1
 
@@ -59,26 +58,10 @@ cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM rece
 rep_lag = cursor.fetchone()[0]
 current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
 
-end = rows_per_page
-page = 1
-for start in range(0, len(output), rows_per_page):
-    report = wikitools.Page(wiki, report_title % page)
-    report_text = report_template % (current_of, '\n'.join(output[start:end]))
-    report_text = report_text.encode('utf-8')
-    report.edit(report_text, summary=settings.editsumm, bot=1)
-    page += 1
-    end += rows_per_page
-
-page = math.ceil(len(output) / float(rows_per_page)) + 1
-while 1:
-    report = wikitools.Page(wiki, report_title % page)
-    report_text = settings.blankcontent
-    report_text = report_text.encode('utf-8')
-    if not report.exists:
-        break
-    report.edit(report_text, summary=settings.blanksumm, bot=1)
-    page += 1
-
+report = wikitools.Page(wiki, report_title)
+report_text = report_template % (current_of, '\n'.join(output))
+report_text = report_text.encode('utf-8')
+report.edit(report_text, summary=settings.editsumm, bot=1)
 
 cursor.close()
 conn.close()
