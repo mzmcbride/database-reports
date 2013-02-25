5885b8e650d4cfcde420d0be658a4b7d09bafb36
Tim Landscheidt <tim@tim-landscheidt.de>
2013-02-25 19:05:58 +0000

    Convert general.confirmedusers to the distributor concept.

---
+++ reports/general/confirmedusers.py
@@ -0,0 +1,69 @@
+# Copyright 2009, 2013 bjweeks, MZMcBride, Tim Landscheidt
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Report class for autoconfirmed users in the confirmed user group
+"""
+
+import datetime
+
+import reports
+
+class report(reports.report):
+    def get_title(self):
+        return 'Autoconfirmed users in the confirmed user group'
+
+    def get_preamble(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+        rep_lag = cursor.fetchone()[0]
+        current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+        return '''Users in the "confirmed" user group who have passed the autoconfirmed threshold; \
+data as of <onlyinclude>%s</onlyinclude>.''' % current_of
+
+    def get_table_columns(self):
+        return ['User', 'Edit count', 'First edit']
+
+    def get_table_rows(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('''
+        /* confirmedusers.py SLOW_OK */
+        SELECT
+          CONVERT(user_name USING utf8),
+          user_editcount,
+          rev_timestamp
+        FROM user
+        JOIN user_groups
+        ON ug_user = user_id
+        JOIN revision
+        ON rev_user = user_id
+        AND ug_group = 'confirmed'
+        AND user_editcount > 9
+        AND (SELECT
+               MIN(rev_timestamp)
+             FROM revision
+             WHERE rev_user = user_id) < DATE_FORMAT(DATE_SUB(NOW(),INTERVAL 4 DAY),'%Y%m%d%H%i%s')
+        AND rev_timestamp = (SELECT
+                               MIN(rev_timestamp)
+                             FROM revision
+                             WHERE rev_user = user_id);
+        ''')
+
+        for user_name, user_editcount, rev_timestamp in cursor:
+            user_name = u'{{dbr link|1=%s}}' % user_name
+            yield [user_name, user_editcount, rev_timestamp]
+
+        cursor.close()
