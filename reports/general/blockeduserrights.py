502868e15f99685661402a441b8ad5fbd207b022
Tim Landscheidt <tim@tim-landscheidt.de>
2013-02-27 23:40:57 +0000

    Convert general.blockeduserrights to the distributor concept.

---
+++ reports/general/blockeduserrights.py
@@ -0,0 +1,63 @@
+# Copyright 2009, 2013 bjweeks, MZMcBride, Tim Landscheidt
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Report class for blocked users in user groups
+"""
+
+import datetime
+
+import reports
+
+class report(reports.report):
+    def get_title(self):
+        return 'Blocked users in user groups'
+
+    def get_preamble(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+        rep_lag = cursor.fetchone()[0]
+        current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+        return '''Users in user groups who are currently blocked; data as of \
+<onlyinclude>%s</onlyinclude>.''' % current_of
+
+    def get_table_columns(self):
+        return ['User', 'User groups', 'Blocker', 'Expiry', 'Reason']
+
+    def get_table_rows(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('''
+        /* blockeduserrights.py SLOW_OK */
+        SELECT
+          CONVERT(user_name USING utf8),
+          CONVERT(ug_group USING utf8),
+          CONVERT(ipb_by_text USING utf8),
+          CONVERT(ipb_expiry USING utf8),
+          CONVERT(ipb_reason USING utf8)
+        FROM user
+        JOIN ipblocks
+        ON user_id = ipb_user
+        JOIN user_groups
+        ON user_id = ug_user;
+        ''')
+
+        for user_name, user_groups, ipb_by_text, ipb_expiry, ipb_reason in cursor:
+            user_name = u'[[User:%s|]]' % user_name
+            ipb_by_text = u'[[User talk:%s|]]' % ipb_by_text
+            ipb_reason = u'<nowiki>%s</nowiki>' % ipb_reason
+            yield [user_name, user_groups, ipb_by_text, ipb_expiry, ipb_reason]
+
+        cursor.close()
