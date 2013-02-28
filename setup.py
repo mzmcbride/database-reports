243466392aa0193f4cb73975c75a51d640e76641
Tim Landscheidt <tim@tim-landscheidt.de>
2013-02-28 19:54:47 +0000

    Convert enwiki.brokenwikiprojtemps to the distributor concept.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='dbreps',
       requires=['oursql',
                 'wikitools'],
       packages=['reports',
+                'reports.enwiki',
                 'reports.general',
                 'reports.tests'],
       scripts=['dbreps'] + [file for dir in ['commonswiki', 'enwiki', 'general', 'plwiki', 'wikidatawiki'] for file in glob.glob(dir + '/*.py')]
