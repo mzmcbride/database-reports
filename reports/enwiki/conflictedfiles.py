9f88ba3a0091bcbaeac56c41d1b09e032aa88082
Tim Landscheidt <tim@tim-landscheidt.de>
2013-03-01 03:17:47 +0000

    Convert enwiki.conflictedfiles to the distributor concept.

---
+++ reports/enwiki/conflictedfiles.py
@@ -0,0 +1,64 @@
+# Copyright 2010, 2013 bjweeks, MZMcBride, Tim Landscheidt
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Report class for files with conflicting categorization
+"""
+
+import datetime
+
+import reports
+
+class report(reports.report):
+    def get_title(self):
+        return 'Files with conflicting categorization'
+
+    def get_preamble(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
+        rep_lag = cursor.fetchone()[0]
+        current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
+
+        return u'''Files that are categorized in [[:Category:All non-free media]] and \
+[[:Category:All free media]]; data as of <onlyinclude>%s</onlyinclude>.''' % current_of
+
+    def get_table_columns(self):
+        return ['File']
+
+    def get_table_rows(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('''
+        /* conflictedfiles.py SLOW_OK */
+        SELECT
+          CONVERT(ns_name USING utf8),
+          CONVERT(page_title USING utf8)
+        FROM page
+        JOIN toolserver.namespace
+        ON dbname = CONCAT(?, '_p')
+        AND page_namespace = ns_id
+        JOIN categorylinks AS c1
+        ON c1.cl_from = page_id
+        JOIN categorylinks AS c2
+        ON c2.cl_from = page_id
+        WHERE page_namespace = 6
+        AND c1.cl_to = 'All_free_media'
+        AND c2.cl_to = 'All_non-free_media';
+        ''' , (self.site, ))
+
+        for ns_name, page_title in cursor:
+            full_page_title = u'[[:%s:%s|%s]]' % (ns_name, page_title, page_title)
+            yield [full_page_title]
+
+        cursor.close()
