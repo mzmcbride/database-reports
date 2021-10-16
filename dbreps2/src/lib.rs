247b44e7f1b1225b15fe0a00080ab4f6e3aa32aa
Kunal Mehta <legoktm@debian.org>
2021-10-15 22:20:46 -0700

    Add a bit of skew in needs_update() for more consistent update times
    E.g. if the report ran at 1:02 UTC and now it's 1:01 UTC the next day,
    we want to re-run the report rather than waiting another hour.

--- dbreps2/src/lib.rs
+++ dbreps2/src/lib.rs
@@ -138,7 +138,8 @@ pub trait Report<T: Send + Sync> {
         };
         let dt = Utc.datetime_from_str(&ts, "%H:%M, %d %B %Y (UTC)")?;
         let now = Utc::now();
-        if (dt + self.frequency().to_duration()) < now {
+        let skew = Duration::minutes(20);
+        if (dt + self.frequency().to_duration() + skew) < now {
             Ok(true)
         } else {
             Ok(false)
